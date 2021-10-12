755802cfa302f396031b247c20b13b60cfd3963c
Đào Lê Bảo Minh <dlbm1302@gmail.com>
2021-10-12 15:29:44 +0700

    updates

---
+++ tpl.cpp
@@ -0,0 +1,16 @@
+/**
+ *    author:  $%U%$
+ *    created: $%D%$.$%M%$.$%Y%$ $%h%$:$%m%$:$%s%$       
+**/
+
+#include <bits/stdc++.h>
+#ifndef LBM_LOCAL
+#define debug(x...) 17
+#endif
+
+using namespace std;
+
+int main() {
+	cin.tie(nullptr) -> sync_with_stdio(false);
+
+}
