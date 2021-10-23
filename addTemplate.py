b35e0a600c6b9d3a074b42201da19629b8727f2f
Đào Lê Bảo Minh <dlbm1302@gmail.com>
2021-10-23 20:24:59 +0700

    Update addTemplate.py

--- addTemplate.py
+++ addTemplate.py
@@ -9,7 +9,7 @@ class InsertTemplOne(sublime_plugin.TextCommand):
         })
 class InsertTemplTwo(sublime_plugin.TextCommand):
     def run(self, edit):
-        date = time.strftime("%d.%m.%Y")
+        date = time.strftime("%d.%m.%Y %H:%M:%S")
         self.view.run_command("insert_snippet", {
             "name": "Packages/User/____VOJ.sublime-snippet",
             "DATE": date
