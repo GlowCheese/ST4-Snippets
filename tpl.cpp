92414cb1e2a6710ddf629745457527b0cf7ed5f2
Đào Lê Bảo Minh <dlbm1302@gmail.com>
2021-10-25 18:24:01 +0700

    noth

--- tpl.cpp
+++ tpl.cpp
@@ -8,9 +8,21 @@
 #define debug(x...) 17
 #endif
 
-using namespace std;
+#define all(a) (a).begin(), (a).end()
+#define For(i, a, b) for (int i = a; i < b; i++)
+#define Rof(i, a, b) for (int i = a; --i >= b; )
 
+using namespace std;           using ll  = long long;
+using str = string;            using ld  = long double;
+using pii = pair<int, int>;    using pll = pair<ll, ll>;
+
+const ll mod = 1e9 + 7,        luv = 18 * 13;
+const ll e30 = 1ll << 30,      e60 = 1ll << 60;
+
+
+
+/* -----------------[ MAIN CODE GOES HERE ]----------------- */
 int main() {
-	cin.tie(nullptr) -> sync_with_stdio(false);
-
+    cin.tie(nullptr) -> sync_with_stdio(false);
+    
 }
