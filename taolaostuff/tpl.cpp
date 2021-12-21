680c424f5a254800f789c49ad02f8c14e5a8a56f
Đào Lê Bảo Minh <dlbm1302@gmail.com>
2021-12-21 09:31:11 +0700

    Create tpl.cpp

---
+++ taolaostuff/tpl.cpp
@@ -0,0 +1,28 @@
+/**
+ *    author:  Dao Le Bao Minh
+ *    created: $%D%$.$%M%$.$%Y%$ $%h%$:$%m%$:$%s%$       
+**/
+
+#include <bits/stdc++.h>
+#ifndef LBM_LOCAL
+#define debug(x...) 17
+#endif
+
+#define all(a)  (a).begin(),  (a).end()
+#define For(i, a, b) for (int i = a; i < b; i++)
+#define Rof(i, a, b) for (int i = a; --i >= b; )
+
+using namespace std;           using ll  = long long;
+using str = string;            using ld  = long double;
+using pii = pair<int, int>;    using pll = pair<ll, ll>;
+
+const ll mod = 1e9 + 7,        luv = 17 * 83;
+const ll e30 = 1ll << 30,      e60 = 1ll << 60;
+
+
+
+/* -----------------[ MAIN CODE GOES HERE ]----------------- */
+int main() {
+    cin.tie(nullptr) -> sync_with_stdio(false);
+    
+}
