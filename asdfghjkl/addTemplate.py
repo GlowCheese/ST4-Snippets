f779429899eae213a1316e2472913ce948033780
Dao Le Bao Minh <djnner@proton.me>
2023-01-25 16:11:35 +0700

    Khai upd đầu năm :D

--- asdfghjkl/addTemplate.py
+++ asdfghjkl/addTemplate.py
@@ -1,20 +1,13 @@
 import sublime, sublime_plugin, time
 
-class InsertTemplOne(sublime_plugin.TextCommand):
+class InsertMainTempl(sublime_plugin.TextCommand):
     def run(self, edit):
         date = time.strftime("%d.%m.%Y")
         self.view.run_command("insert_snippet", {
             "name": "Packages/User/____main.sublime-snippet",
             "DATE": date
         })
-class InsertTemplTwo(sublime_plugin.TextCommand):
-    def run(self, edit):
-        date = time.strftime("%d.%m.%Y %H:%M:%S")
-        self.view.run_command("insert_snippet", {
-            "name": "Packages/User/____VOJ.sublime-snippet",
-            "DATE": date
-        })
-class InsertTemplThree(sublime_plugin.TextCommand):
+class InsertMttTempl(sublime_plugin.TextCommand):
     def run(self, edit):
         date = time.strftime("%d.%m.%Y")
         self.view.run_command("insert_snippet", {
